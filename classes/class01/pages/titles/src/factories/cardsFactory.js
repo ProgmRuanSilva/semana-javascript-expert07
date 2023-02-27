f50ec5d37614567c795dc528e4fe9f2a370bb487
Ruan Silva <progmruansilva@gmail.com>
2023-02-27 13:52:01 -0300

    refatoring

--- classes/class01/pages/titles/src/factories/cardsFactory.js
+++ classes/class01/pages/titles/src/factories/cardsFactory.js
@@ -1,6 +1,6 @@
-import CardsController from "./../controllers/cardsController.js"
-import CardsView from "./../views/cardsView.js"
-import CardsService from "./../services/cardsService.js"
+import CardsController from "../controllers/cardsController.js"
+import CardsView from "../views/cardsView.js"
+import CardsService from "../services/cardsService.js"
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initalize() {
